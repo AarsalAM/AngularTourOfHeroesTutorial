d068700eab514eb555982f01f363c8c0b7b66d33
AarsalM <91852812+AarsalM@users.noreply.github.com>
2023-04-17 14:24:31 -0400

    details page now works and routes properly

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,6 +11,27 @@ import { Hero } from '../hero';
 })
 export class HeroDetailComponent {
 
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
   @Input() hero?: Hero;
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+  // For now, assume that a hero with the specified `id` always exists.
+  // Error handling will be added in the next step of the tutorial.
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 
 }
